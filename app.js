fdb1f925844fadfbc3eb509903d7beebe8804d3c
Sicktooth <sicktooth003@gmail.com>
2024-11-21 16:59:05 +0100

    minor change

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const ejs = require("ejs");
 const PORT = 3000;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
 const {connectDB} = require('./config/database')
 const {createDocument, destroyArticles, getArticles} = require('./routes/all')
 const {getTitle, putTitle, patchTitle, destroyDoc} = require('./routes/specific')
@@ -24,7 +22,7 @@ app.route('/articles/:articleTitle')
     .get(getTitle)
     .put(putTitle)
     .patch(patchTitle)
-    .delete(destroyDoc)
+.delete(destroyDoc)
 
 app.listen(PORT, ()=>{
     console.log("server started at localhost:" + PORT);
